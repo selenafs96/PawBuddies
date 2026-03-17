ee4d43029910db1c9e5c703a88d8974da4bbfab0
Trabajo <selenaferrando@gmail.com>
2026-03-17 16:03:58 +0100

    Creados archivos de configuración de supabase y realizada prueba fetching de datos en App.js

--- PawBuddies/App.js
+++ PawBuddies/App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { supabase } from './lib/supabase.js'
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+  async function fetchData() {
+    const {data: result, error} = await supabase.from('protectora').select('*');
+    if (error) {
+      console.log(error);
+    } else {
+      setData(result);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {data.map((item, index) => (
+        <Text key={item.id_protectora}>{item.nombre}: {item.presentacion}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
