be25371bea8713fe0d2c8a2ff587c46f522f01bd
casa <selenaferrando@gmail.com>
2026-03-23 19:20:42 +0100

    Limpiado App.js y añadido archivo ejemplo fetching de datos a parte

--- PawBuddies/App.js
+++ PawBuddies/App.js
@@ -1,29 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { supabase } from './lib/supabase.js'
-import { useEffect, useState } from 'react';
 
 export default function App() {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData()
-  }, []);
-
-  async function fetchData() {
-    const {data: result, error} = await supabase.from('protectora').select('*');
-    if (error) {
-      console.log(error);
-    } else {
-      setData(result);
-    }
-  }
   return (
     <View style={styles.container}>
-      {data.map((item, index) => (
-        <Text key={item.id_protectora}>{item.nombre}: {item.presentacion}</Text>
-      ))}
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
