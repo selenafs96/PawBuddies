7846d16670314def1c5dfe9fe6684bd79a6dcb47
Trabajo <selenaferrando@gmail.com>
2026-03-26 16:26:55 +0100

    layor condigurado con safeareaprovider y statusbar

--- PawBuddies/app/_layout.jsx
+++ PawBuddies/app/_layout.jsx
@@ -6,11 +6,11 @@ import { StatusBar } from 'expo-status-bar';
 const RootLayout = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar style='auto'/>
-      <Text>_layout</Text>
-      <Slot />
+      <StatusBar style="auto" />
+        <Text>_layout</Text>
+        <Slot />
     </SafeAreaProvider>
-  )
-}
+  );
+};
 
 export default RootLayout;
