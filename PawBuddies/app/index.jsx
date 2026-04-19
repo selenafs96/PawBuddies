c6cdef1cc2a878a072bcb970ad28552ea44827f2
Thalyssien <146372113+Thalyssien@users.noreply.github.com>
2026-04-19 20:40:09 +0200

    Añadido import registroAdoptantes
    Se añade el import registroAdoptantes al Index para tests.

--- PawBuddies/app/index.jsx
+++ PawBuddies/app/index.jsx
@@ -3,6 +3,7 @@ import Onboarding4 from './(adopters)/onboarding4.jsx';
 import AdoptaScreen from './(animals)/list.jsx';
 import VolunteerProfile from './(volunteers)/profile.jsx';
 import RegistroAnimalesScreen from './(volunteers)/RegistroAnimalesScreen.jsx';
+import registroAdoptantes from './(volunteers)/registroAdoptantes.jsx';
 
 // const PawBuddies = () => {
 
